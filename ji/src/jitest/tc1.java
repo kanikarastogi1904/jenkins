ccc15b9b75e2bb8fdbf762702658d9bd81dac492
HP <HP@LAPTOP-SAISLCIH>
2022-07-17 13:13:55 +0530

    pushing the code first time

---
+++ ji/src/jitest/tc1.java
@@ -0,0 +1,19 @@
+package jitest;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class tc1 {
+	
+	
+
+	@Test
+	public void testc1()
+	{
+		System.out.println("*********************************test case");
+	}
+	
+
+	
+}
